2d58ba8b82d04851afab99f4d0160be8b6593396
mperdeck <matt.perdeck@readify.net>
2017-05-26 23:05:27 +1000

    trying reading into datatable. failed

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Spire.Xls;
+using System.Windows.Forms;
 
 namespace WebApplication1.Controllers
 {
@@ -10,6 +12,33 @@ namespace WebApplication1.Controllers
     {
         public IActionResult Index()
         {
+            // See
+            // https://www.nuget.org/packages/FreeSpire.XLS/7.9.1
+            // 
+            // Doco:
+            // https://www.e-iceblue.com/Tutorials/Spire.XLS/Spire.XLS-Program-Guide/Worksheet/Get-a-list-of-the-worksheet-names-in-an-Excel-workbook.html
+
+            string xlsxPath = @"C:\Dev\CoreAndLegacy\test.xlsx";
+
+            Workbook workbook = new Workbook();
+            workbook.LoadFromFile(xlsxPath);
+
+            Worksheet sheet = workbook.Worksheets[0];
+            var dataGridView = new DataGridView();
+
+            dataGridView.DataSource = sheet.ExportDataTable(sheet.FirstRow, sheet.FirstColumn, sheet.LastRow, sheet.LastColumn, false);
+
+            foreach (DataGridViewRow row in dataGridView.Rows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    string value = cell.Value.ToString();
+
+                }
+            }
+
+
+
             return View();
         }
 

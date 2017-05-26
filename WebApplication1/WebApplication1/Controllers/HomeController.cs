4d96e53d1589025f8e3403fe9bf62a759a2574bb
mperdeck <matt.perdeck@readify.net>
2017-05-26 23:27:15 +1000

    accessing the .xslx by reading the work sheet directly

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
+// CORE namespaces
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+// namespace exported by NuGet package that does not support CORE
 using Spire.Xls;
-using System.Windows.Forms;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        // ILogger is a CORE feature
+        private readonly ILogger _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             // See
@@ -18,26 +26,28 @@ namespace WebApplication1.Controllers
             // Doco:
             // https://www.e-iceblue.com/Tutorials/Spire.XLS/Spire.XLS-Program-Guide/Worksheet/Get-a-list-of-the-worksheet-names-in-an-Excel-workbook.html
 
+            // Here we read an .xlsx file and write its contents to the CORE logger.
+            // The CORE logger in turn writes to the debug window.
+
             string xlsxPath = @"C:\Dev\CoreAndLegacy\test.xlsx";
 
+            // Workbook is defined in the non-CORE package Spire.Xls
             Workbook workbook = new Workbook();
             workbook.LoadFromFile(xlsxPath);
 
             Worksheet sheet = workbook.Worksheets[0];
-            var dataGridView = new DataGridView();
 
-            dataGridView.DataSource = sheet.ExportDataTable(sheet.FirstRow, sheet.FirstColumn, sheet.LastRow, sheet.LastColumn, false);
-
-            foreach (DataGridViewRow row in dataGridView.Rows)
+            CellRange[] rows = sheet.Rows;
+            foreach(CellRange row in rows)
             {
-                foreach (DataGridViewCell cell in row.Cells)
-                {
-                    string value = cell.Value.ToString();
+                _logger.LogInformation("----------- next row -----------");
 
+                foreach (CellRange cell in row.Cells)
+                {
+                    string cellValue = cell.Value;
+                    _logger.LogInformation(cellValue);
                 }
             }
-
-
 
             return View();
         }
